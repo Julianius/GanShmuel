433e6a5d4a06c418c6f06dae83a22a5a808503d4
Julian <julianmotorbreathe@gmail.com>
2021-09-09 15:03:34 +0300

    Changed ci.py

--- devops/ci.py
+++ devops/ci.py
@@ -13,8 +13,5 @@ def github_webhook_endpoint():
         
   return str(data.get("repository").get("name"))
 
-
-
-
 if __name__ == "__main__":
-  app.run()
+    app.run(host="0.0.0.0", port=8080, debug=True)
