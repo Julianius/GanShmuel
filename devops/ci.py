b74e942aebdbf5020f8b57103fb1aacd37378ef6
Julian <julianmotorbreathe@gmail.com>
2021-09-09 15:57:43 +0300

    Testing

--- devops/ci.py
+++ devops/ci.py
@@ -15,6 +15,6 @@ def github_webhook_endpoint():
 
   os.system('git clone "https://github.com/Julianius/GanShmuel" origin ' + branch_name) 
   return "OK"
-
+#asdasdasd
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
