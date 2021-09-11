6d8937c867609dfc9669e8235bce9d83d9ecc30d
Julian <julianmotorbreathe@gmail.com>
2021-09-11 11:28:33 +0300

    Fixed some stuff

--- devops/ci.py
+++ devops/ci.py
@@ -9,6 +9,7 @@ PORTS = { 'devops-production': '8080', 'billing-staging': '8081', 'billing-produ
 REPO = 'https://github.com/Julianius/GanShmuel.git'
 #PATHS = { 'local': '/home/julian/GanShmuel/', 'remote': '/home/ec2-user/GanShmuel/' }
 
+
 @app.route('/health', methods=['GET'])
 def health():
     return '200'
@@ -16,24 +17,23 @@ def health():
 @app.route("/myhook", methods=['POST'])
 def github_webhook_endpoint():
 
+
+
   # Check the os type to ensure that it is local or remote
-  os_type_cmd='cat /etc/os-release | grep NAME | head -n 1 | cut -d \\" -f 2'
-  OS_TYPE = subprocess.check_output(os_type_cmd, shell=True)
-  print(OS_TYPE)
-  if 'Ubuntu' in str(OS_TYPE):
-    path = 'local'
-  else:
-    path = 'remote'  
+  #os_type_cmd='cat /etc/os-release | grep NAME | head -n 1 | cut -d \\" -f 2'
+  #OS_TYPE = subprocess.check_output(os_type_cmd, shell=True)
+  #print(OS_TYPE)
+  #if 'Ubuntu' in str(OS_TYPE):
+  #  path = 'local'
+  #else:
+  #  path = 'remote'  
   #path = PATHS.get(path)
-  path = '/GanShmuel'
+  path = '/GanShmuel/app'
   data = request.get_json()
   branch_name = data.get('ref').split('/')[-1] 
 
   #os.system('cat /etc/os/release')
 
-
-
-  
   if branch_name in BRANCHES:
     if branch_name == 'main':
       os.system('git clone ' + REPO + ' ' + path + '/temp')
@@ -43,7 +43,7 @@ def github_webhook_endpoint():
         os.system('mkdir -p ' + path + 'main-production')
         os.system('mv '+ path + 'temp/* ' + path + 'temp/.* ' + path + 'main-production/ 2>/dev/null')
       os.system('rm -rf ' + path + 'temp')
-      os.system('docker-compose -f /GanShmuel/apps/billing-staging/GanShmuel/billing/Prod/docker-compose.yml up -d --force-recreate')
+      os.system('docker-compose -f ' + path + 'main-production/billing/docker-compose.yml up -d --force-recreate')
     else:
       print('b')
 
