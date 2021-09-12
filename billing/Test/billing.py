24cf57b17f914a34c25ef7a3ff961549d5fd701b
kfirosb <kfirosb@gmail.com>
2021-09-12 10:54:19 +0300

    add databases,provider.

--- billing/Test/billing.py
+++ billing/Test/billing.py
@@ -1,22 +1,8 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from flask import Response
 import mysql.connector
-
-ifconnect = False
-try:
-    billingdb = mysql.connector.connect(
-    host="billingdb",
-    user="root",
-    password="1234!",
-    )
-    mycursor = billingdb.cursor()
-    mycursor.execute("select 1")
-    myresult = mycursor.fetchall()
-    billingdb.commit()
-    ifconnect = True
-except:
-    ifconnect = False
-
+import json
+import requests
 
 app = Flask(__name__)
 
@@ -27,60 +13,61 @@ def index():
 @app.route('/health')
 def health():
     if ifconnect == True:
-        return Response({"Ok"},status=200)
+        return Response({"Ok"}, status=200)
     else:
-        return Response({"Internal server error"},status=500)
+        return Response({"Internal server error"}, status=500)
 
+@app.route('/provider')
+def provider():
+    return render_template('providers.html')
+
+@app.route('/provider', methods=['GET', 'POST'])
+def providers():
+    if request.method == 'POST':
+        provider = request.form['provider']
+        provid = request.form["provid"]
+        jsonprovider = {"id": provid, "name": provider}
+        with open(f"./templates/providerjson.json", "a+") as chat_file:
+            chat_file.write(str(jsonprovider))
+        billingdb = mysql.connector.connect(
+            host="billingdb",
+            user="root",
+            password="1234!",
+            database='billdb',
+        )
+        mycursor = billingdb.cursor()
+        mycursor.execute("USE billdb")
+        # mycursor.execute(f"SELECT 'name' FROM Provider")
+        # result = mycursor.fetchone()
+        # if provider not in result:
+        mycursor.execute(f"INSERT INTO Provider(id, name) VALUES('{str(provid)}', '{str(provider)}')")
+        return requests.post('localhost:8081/provider', json={'id': 1, 'name': ''})
+
+    elif request.method == 'GET':
+        return Response({"enter"}, mimetype='text/plain')
 
 if __name__ == '__main__':
+    ifconnect = False
+    billingdb = mysql.connector.connect(
+        host="billingdb",
+        user="root",
+        password="1234!",
+    )
+    mycursor = billingdb.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS billdb")
+    mycursor.execute("USE billdb")
+    mycursor.execute(
+        "CREATE TABLE IF NOT EXISTS Provider (id int(11) NOT NULL AUTO_INCREMENT,name varchar(255) DEFAULT NULL,PRIMARY KEY (id)) ENGINE=MyISAM  AUTO_INCREMENT=10001")
+    mycursor.execute(
+        "CREATE TABLE IF NOT EXISTS Rates (product_id varchar(50) NOT NULL,rate int(11) DEFAULT 0,scope varchar(50) DEFAULT NULL,FOREIGN KEY (scope) REFERENCES Provider (id)) ENGINE=MyISAM")
+    mycursor.execute(
+        "CREATE TABLE IF NOT EXISTS Trucks (id varchar(10) NOT NULL,provider_id int(11) DEFAULT NULL,PRIMARY KEY (id),FOREIGN KEY (provider_id) REFERENCES Provider (id)) ENGINE=MyISAM")
+
+    try:
+        myresult = mycursor.fetchall()
+        billingdb.commit()
+        ifconnect = True
+    except:
+        ifconnect = False
+
     app.run(debug=True, port=8081, host='0.0.0.0')
-
-
-
-
-
-
-
-
-
-
-# myhostname = socket.gethostname()
-# myipaddr= socket.gethostbyname(socket.gethostname())
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html',myhostname=myhostname,myipaddr=myipaddr)
-
-
-# @app.route('/rates',methods=['GET']
-#     with open (nativ shel jason, 'r'):
-
-
-#     return render_template('rates.html',read jason)
-
-
-# @app.route('/<roomopen>')
-# def roomsesh(roomopen):
-#     return render_template('index.html',myhostname=myhostname,myipaddr=myipaddr)
-
-# @app.route('/api/chat/<room>', methods=['GET'])
-# def roomread(room):
-#     try:
-#         with open(f"./chat/chat{room}.txt", "r") as readchat:
-#             readchat_is = readchat.read()
-#             return  Response(readchat_is , mimetype='text/plain')
-#     except:
-#         with open(f"./chat/chat{room}.txt", "w") as readchat:
-#             return f"Created chat room: {room} "
-
-# @app.route('/api/chat/<room>', methods=['POST'])
-# def roomwrite(room):
-#     user=request.form['username']
-#     msg=request.form["msg"]
-#     with open(f"./chat/chat{room}.txt", "a") as chatwrite:
-#         chatwrite.write(f"{user}: {msg}")
-#         chatwrite.write("\n")
-#     return "good job"       
-
-# if __name__ == '__main__':
-#     app.run(debug=True, port=9191, host='0.0.0.0')
