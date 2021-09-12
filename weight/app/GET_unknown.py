3cfcb18d14ea35e1e81fb6ff2b46663f5157244f
Your Name <yaelkadosh>
2021-09-12 18:12:05 +0300

    tast

--- weight/app/GET_unknown.py
+++ weight/app/GET_unknown.py
@@ -1,11 +1,9 @@
 from mysql_db import mysql_db
-
 def GET_unknown():
 	mySQL = mysql_db()
 	try:
 		print("DB error")
 		info = mySQL.getData("SELECT distinct id FROM containers WHERE weight IS NULL")		
 		return '\n'.join(map(str,info))
-		
 	except:		
 		return "Weight data is ok"
