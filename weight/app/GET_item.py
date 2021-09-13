4be74041a170f6be657ddb146fa5b6ed8cc42d77
Faresbad <68914683+Faresbad@users.noreply.github.com>
2021-09-13 11:19:29 +0300

    create get-item file

---
+++ weight/app/GET_item.py
@@ -0,0 +1,18 @@
+from mysql_db import mysql_db
+import datetime
+
+def GET_item(id):
+	mysql = mysql_db()
+
+	try:
+		enterTime = mysql.getData("SELECT date FROM sessions WHERE trucks_id = id")
+		exitTime = mysql.getData("SELECT date FROM sessions WHERE direction = 'out'")
+		in_time = enterTime.now().strftime("%Y%m%d%H%M%S")
+		out_time = exitTime.now().strftime("%Y%m%d%H%M%S") 
+		duration = (out_time - in_time)
+
+		return duration
+
+	except:
+		print("Error!!!")
+	return "i got the item data, ok"
