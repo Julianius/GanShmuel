7d0627e6cbb555eb242475117524e7ab50e4d93f
Yoav yung <Joaffzie@gmail.com>
2021-09-13 14:43:56 +0300

    minor changes

--- weight/app/GET_item.py
+++ weight/app/GET_item.py
@@ -1,14 +1,18 @@
 from mysql_db import mysql_db
 import datetime
+from flask import request
 
 def GET_item(id):
 	mysql = mysql_db()
+ 
+	t1 = request.args.get('t1')
+	t2 = request.args.get('t2')
 
 	try:
-		enterTime = mysql.getData("SELECT date FROM sessions WHERE trucks_id = id")
+		enterTime = mysql.getData(f"SELECT date FROM sessions WHERE trucks_id = {id}")
 		exitTime = mysql.getData("SELECT date FROM sessions WHERE direction = 'out'")
-		in_time = enterTime.now().strftime("%Y%m%d%H%M%S")
-		out_time = exitTime.now().strftime("%Y%m%d%H%M%S") 
+		in_time = enterTime[0]['date']
+		out_time = exitTime[0]['date']
 		if(out_time >= in_time):
 			duration = (out_time - in_time)
 			return duration
--- weight/app/app.py
+++ weight/app/app.py
@@ -3,6 +3,7 @@ from flask import Flask, Response, request
 import requests
 from GET_health import GET_health
 from GET_unknown import GET_unknown
+from GET_item import GET_item
 
 app = Flask(__name__)
 
@@ -18,7 +19,7 @@ def health():
 def unknown_weight():
     return GET_unknown()
 
-@app.route("/batch-weight/<file>", methods=['POST'])
+@app.route("/batch-weight/<file>", methods=['POST', 'GET'])
 def batch_weight(file):
     return POST_batch_weight(file)
 
@@ -28,7 +29,7 @@ def weight_get_weight():
 
 @app.route("/item/<id>", methods=['GET']) 
 def item_weight(id): 
-    return "to do"
+    return GET_item(id)
 
 @app.route("/weight", methods=['POST']) 
 def weight_post_weight(): 
