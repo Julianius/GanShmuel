ef3ae6474e4c97343aaad9f739fff919a1377e2b
Izhak <izhaklatovski@gmail.com>
2021-09-13 13:40:04 +0300

    Monitoring test

--- devops/ci.py
+++ devops/ci.py
@@ -48,7 +48,7 @@ def build_app(branch_name, merger_name, pusher):
       os.environ["DYNAMIC_PORT"] = "8081"
       os.system('docker-compose -f ' + PATH + branch_name + '/billing/Prod/docker-compose.yml -p billing-staging up -d --build --force-recreate')
 
-@app.route('/', methods=['GET'])
+@app.route('/monitor', methods=['GET'])
 def home():
     return 'fgfgfg'
 
