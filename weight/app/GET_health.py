f511c1bf5e65d1d32067a2c89c9b4663b51ef8c3
Your Name <yaelkadosh>
2021-09-14 16:13:20 +0300

    allApi

--- weight/app/GET_health.py
+++ weight/app/GET_health.py
@@ -7,11 +7,7 @@ TODO - check POST services
 import requests
 
 def GET_health():
-<<<<<<< HEAD
-	services = {"unknown","batch-weight/id","item/id"}
-=======
 	services = {"unknown","batch-weight/id","item/id","session/id"}
->>>>>>> 273c5c8cf607147a75f016a148e67b0a5d0cc1cd
 	result = ""
 	for service in services:
 		req = requests.get(f"http://localhost:5000/{service}")
