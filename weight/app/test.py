6452381a260b92b47acf72ded496653e07b55a27
sapsap1 <sapiralon95@gmail.com>
2021-09-17 16:00:29 +0300

    test

--- weight/app/test.py
+++ weight/app/test.py
@@ -1,53 +1,43 @@
-import unittest
-from app import app
-class BasicTestCase(unittest.TestCase):
-        def test_home(self):
-                tester = app.test_client(self)
-                response = tester.get('/', content_type='html/text')
-                self.assertEqual(response.status_code, 200)
-                self.assertEqual(response.data, b'Flask app - Blue team Weight ')
-        def test_GET_health(self):
-                tester = app.test_client(self)
-                response = tester.get('/health', content_type='html/text')
-                print(response.status_code)
-                self.assertEqual(response.status_code, 200)
-                self.assertTrue(b'ok' in response.data)
-        def test_GET_unknown(self):
-                tester = app.test_client(self)
-                response = tester.get('/unknown', content_type='html/text')
-                print(response.status_code)
-                self.assertEqual(response.status_code, 200)
-                self.assertTrue(b'Weight data is unavailable at the moment.' not in response.data)
-        def test_POST_batch_weight(self):
-                tester = app.test_client(self)
-                response = tester.get('/batch-weight/test', content_type='html/text')
-                print(response.status_code)
-                self.assertEqual(response.status_code, 200)
-                print(response.data)
-                self.assertTrue(b'Has been added to database.' in response.data)
-        def test_GET_weight_weight(self):
-                tester = app.test_client(self)
-                response = tester.get('/weight', content_type='html/text')
-                print(response.status_code)
-                self.assertEqual(response.status_code, 200)
-                self.assertTrue(b'erorr db' not in response.data)
-        def test_GET_item(self):
-                tester = app.test_client(self)
-                response = tester.get('item/2', content_type='html/text')
-                print(response.status_code)
-                self.assertEqual(response.status_code, 200)
-                self.assertTrue(b'erorr db' not in response.data)
-        def test_GET_session(self):
-                tester = app.test_client(self)
-                response = tester.get('session/2', content_type='html/text')
-                print(response.status_code)
-                self.assertEqual(response.status_code, 200)
-                self.assertTrue(b'erorr db' not in response.data)
-        def test_POST_weight_weight(self):
-                tester = app.test_client(self)
-                response = tester.get('/weight', content_type='html/text')
-                print(response.status_code)
-                self.assertEqual(response.status_code, 200)
-                self.assertTrue(b'erorr db' not in response.data)
+"""
+TODO Update health - return value
+"""
+import requests
+def test_home():
+        req = requests.get("18.157.175.199:8085")
+        status_code = req.status_code
+        if (status_code < 200 or status_code > 299) and (req.text=='Flask app - Blue team Weight '):
+                return 1			
+        else:
+                return 0
+# def test_health():
+#         req = requests.get("http://localhost:8083/health")
+#         status_code = req.status_code
+#         if (status_code < 200 or status_code > 299) and (req.text=='Service item/id : ... ok Service unknown : ... ok Service batch-weight/id : ... ok Service session/id : ... ok '):
+#                 return 1			
+#         else:
+#                 return 0
+def test_unknown():
+        req = requests.get("18.157.175.199:8085/unknown")
+        status_code = req.status_code
+        if (status_code < 200 or status_code > 299) and (req.text!='Weight data is unavailable at the moment.'):
+                return 1			
+        else:
+                return 0
+
+# def test_unknown():
+#         req = requests.get("http://localhost:8083/unknown")
+#         status_code = req.status_code
+#         if (status_code < 200 or status_code > 299) and (req.text=='No missing weights found in data base'):
+#                 return 1			
+#         else:
+#                 return 0
+def test():
+        flag=0
+        flag+=test_home()
+        flag+=test_unknown()
+        if flag==2:
+                return 0
+        return 1
+
 if __name__ == '__main__':
-    unittest.main()
+    test()
