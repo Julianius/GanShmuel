dd79325ff1dfba43eabaebe99e28064c982fb0fb
root <root@DESKTOP-QGU5KT0.localdomain>
2021-09-18 20:23:25 +0300

    fix merge

--- weight/app/GET_health.py
+++ weight/app/GET_health.py
@@ -6,19 +6,6 @@ TODO - check POST services
 
 import requests
 
-<<<<<<< HEAD
-def tastget():
-	req = requests.get(f"http://localhost:5000/unknown")
-	if req.status_code < 200 or req.status_code > 299:
-		return 1
-def tast():
-	flag+=tastget()
-	if flag>8:
-		return 1
-	else:
-		return 0
-
-=======
 def GET_health():
 	services = {"unknown","batch_weight/file","item/id","session/id"}
 	result = ""
@@ -30,6 +17,5 @@ def GET_health():
 		else:
 			result += f"\n Service {service} : ... ok \n"
 	return result
->>>>>>> 662024b087cea32a48086d2ba4f38066167be4d3
 if __name__ == '__main__':
     tast()
