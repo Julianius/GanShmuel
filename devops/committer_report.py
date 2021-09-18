550ba803952960ab53485e39a960b73243dbf098
Julian <julianmotorbreathe@gmail.com>
2021-09-18 19:01:57 +0300

    Fix

--- devops/committer_report.py
+++ devops/committer_report.py
@@ -5,13 +5,15 @@ TLD_DEVOPS = 'shekel8@gmail.com'
 TLD_WEIGHT = 'yael260640@gmail.com'
 TLD_BILLING = 'ravivnadiv2@gmail.com'
 TLD_HELPER = 'vjulianiusv@gmail.com'
-PATH = 'GanShmuel/app/commits.txt'
+PATH = '/home/ec2-user/GanShmuel/app/commits.txt'
 
 def send_committer_report():
     text = ''
     with open(PATH, 'r') as reader:
         line = reader.readline()
-        text += line
+        while line:
+            text += line
+            line = reader.readline()
     send_email('Committer report', text, None, None, [TLD_DEVOPS, TLD_BILLING, TLD_WEIGHT, TLD_HELPER])
 
 send_committer_report()
