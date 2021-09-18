1241590f9dfd04276dda8943f0ec782c21cdd2de
sapsap1 <sapiralon95@gmail.com>
2021-09-18 23:05:03 +0300

    3

--- weight/app/test.py
+++ weight/app/test.py
@@ -2,20 +2,13 @@
 TODO Update health - return value
 """
 import requests
-# def test_home():
-#         req = requests.get("18.157.175.199:8085")
-#         status_code = req.status_code
-#         if (status_code < 200 or status_code > 299) and (req.text=='Flask app - Blue team Weight '):
-#                 return 1			
-#         else:
-#                 return 0
-# def test_health():
-#         req = requests.get("http://localhost:8083/health")
-#         status_code = req.status_code
-#         if (status_code < 200 or status_code > 299) and (req.text=='Service item/id : ... ok Service unknown : ... ok Service batch-weight/id : ... ok Service session/id : ... ok '):
-#                 return 1			
-#         else:
-#                 return 0
+def test_health():
+        req = requests.get("http://localhost:8083/health")
+        status_code = req.status_code
+        if (status_code < 200 or status_code > 299) and (req.text=='Service item/id : ... ok Service unknown : ... ok Service batch-weight/id : ... ok Service session/id : ... ok '):
+                return 1			
+        else:
+                return 0
 def test_unknown():
         req = requests.get("18.157.175.199:8085/unknown")
         status_code = req.status_code
@@ -23,14 +16,14 @@ def test_unknown():
                 return 1			
         else:
                 return 0
+def test_item():
+        req = requests.get("18.157.175.199:8085/item/")
+        status_code = req.status_code
+        if (status_code < 200 or status_code > 299) and (req.text!='Weight data is unavailable at the moment.'):
+                return 1			
+        else:
+                return 0
 
-# def test_unknown():
-#         req = requests.get("http://localhost:8083/unknown")
-#         status_code = req.status_code
-#         if (status_code < 200 or status_code > 299) and (req.text=='No missing weights found in data base'):
-#                 return 1			
-#         else:
-#                 return 0
 def test():
         flag=0
         # flag+=test_home()
