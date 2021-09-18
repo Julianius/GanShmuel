912b97738b49d7d48c29dc61450f34d803f7699d
Raviv <nadivravivz@gmail.com>
2021-09-18 10:40:39 -0700

    more fixes

--- billing/Working/Prod/changeport.py
+++ billing/Working/Prod/changeport.py
@@ -5,3 +5,11 @@ with open(f'billing.py', "r") as readchange:
     readit=readit.replace("5000" , str(int(sys.argv[1])+2))
 with open(f'billing.py', "w") as readchange:
     readchange.write(readit)
+listfile=["bills.html","trucks.html","truck_id.html","rates.html","providers.html","index.html","health.html","bills_spec.html"]
+for file in listfile:
+    print(file)
+    with open(f'templates/{file}', "r") as readchange:
+        readit = readchange.read()
+        readit = readit.replace("localhost:8081", "18.157.175.199:"+str(int(sys.argv[1])))
+    with open(f'templates/{file}', "w") as readchange:
+        readchange.write(readit)
